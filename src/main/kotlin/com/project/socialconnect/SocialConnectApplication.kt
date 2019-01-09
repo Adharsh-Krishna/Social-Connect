74fbde3abf7f60f6af2b8dc450390e35a507446d
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-09 19:19:22 +0530

    [Adharsh] Adds Application Driver file

---
+++ src/main/kotlin/com/project/socialconnect/SocialConnectApplication.kt
@@ -0,0 +1,12 @@
+package com.project.socialconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialConnectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocialConnectApplication>(*args)
+}
+
