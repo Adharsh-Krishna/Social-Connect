b754d967b6beb85e067ca8eb954ee4946a3ce34b
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 18:01:32 +0530

    [Adharsh] Adds application test file

---
+++ src/test/kotlin/com/project/socialconnect/SocialConnectApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.project.socialconnect
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SocialConnectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
