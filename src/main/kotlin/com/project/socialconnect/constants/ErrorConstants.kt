731244541e5750feaedfbd6e2e86013e0f77adad
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 19:41:48 +0530

    [Adharsh] Adds certain contants for account controller

--- src/main/kotlin/com/project/socialconnect/constants/ErrorConstants.kt
+++ src/main/kotlin/com/project/socialconnect/constants/ErrorConstants.kt
@@ -5,6 +5,9 @@ abstract class ErrorConstants {
         const val USER_NOT_FOUND = "User could not be found"
         const val USER_ALREADY_EXISTS_WITH_USER_NAME = "User already exists with the user name"
         const val RECORD_COULD_NOT_BE_UPDATED = "Record could not be updated. Unknown error."
+        const val ACCOUNT_TYPE_NOT_FOUND = "Account Type could not be found"
+        const val ACCOUNT_ALREADY_EXISTS = "Account already exists for this user with the given name"
+        const val UNKNOWN_ERROR = "Unknown error"
     }
 
 }
