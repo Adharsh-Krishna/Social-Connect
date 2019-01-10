fedaf695893970db7eb2c754e028824dd2ad6b12
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 17:58:36 +0530

    [Adharsh] Adds User response payload structure

---
+++ src/main/kotlin/com/project/socialconnect/payloads/user/UserResponsePayload.kt
@@ -0,0 +1,6 @@
+package com.project.socialconnect.payloads.user
+
+data class UserResponsePayload(val id: Long?,
+                               val firstName: String?,
+                               val lastName: String?,
+                               val accountsCount: Int?)
