ccdba5aa85e070acaae921ecf925c64be3c72997
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 17:46:02 +0530

    [Adharsh] Adds Account Credential model

---
+++ src/main/kotlin/com/project/socialconnect/models/AccountCredential.kt
@@ -0,0 +1,36 @@
+package com.project.socialconnect.models
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "account_credentials")
+data class AccountCredential(
+        @OneToOne
+        @JoinColumn(name = "account_id", nullable = false)
+        private var account: Account,
+
+        @Column(nullable = false)
+        private var accessToken: String,
+
+        @Column(nullable = false)
+        private var refreshToken: String,
+
+        @Column(nullable = false)
+        private var expiresIn: Long,
+
+        @Column(nullable = false)
+        private var tokenType: String,
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private  var id: Long?) {
+
+    constructor(account: Account, accessToken: String, refreshToken: String, expiresIn: Long, tokenType: String)
+            : this(account = account,
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            expiresIn = expiresIn,
+            tokenType = tokenType,
+            id = null)
+
+}
