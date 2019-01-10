eafb1ffad64e965ead7550c01f57cd35c4f77996
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 19:40:27 +0530

    [Adharsh] Adds ability to find an account using the user id and account name

--- src/main/kotlin/com/project/socialconnect/repositories/AccountRepository.kt
+++ src/main/kotlin/com/project/socialconnect/repositories/AccountRepository.kt
@@ -1,9 +1,17 @@
 package com.project.socialconnect.repositories
 
 import com.project.socialconnect.models.Account
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import java.util.*
+import javax.transaction.Transactional
 
 interface AccountRepository: CrudRepository<Account, Long> {
 
     fun findByName(name: String): Account
+
+    @Transactional
+    @Query("SELECT a.id, a.name, a.accountType FROM Account a INNER JOIN User u ON u.id = :userId AND a.name = :accountName")
+    fun findByUserIdAndAccountName(@Param("userId") userId: Long, @Param("accountName") accountName: String): Optional<Account>
 }
