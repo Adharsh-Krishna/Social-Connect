e7ac4c282046e29a4bac69b48addca98f4188333
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 17:47:57 +0530

    [Adharsh] Adds User repository

---
+++ src/main/kotlin/com/project/socialconnect/repositories/UserRespository.kt
@@ -0,0 +1,22 @@
+package com.project.socialconnect.repositories
+
+
+import com.project.socialconnect.models.User
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import java.util.*
+import javax.transaction.Transactional
+
+interface UserRepository : CrudRepository<User, Long> {
+
+    fun findByUserName(userName: String): Optional<User>
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.firstName = :firstName, u.lastName = :lastName WHERE u.id = :userId")
+    fun updateFirstAndLastName(@Param("userId") userId: Long,
+                               @Param("firstName") firstName: String,
+                               @Param("lastName") lastName: String): Int
+}
