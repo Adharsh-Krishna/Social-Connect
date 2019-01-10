8d7803b35d212050c8dfff02f0b5906e10c09b89
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-10 19:39:48 +0530

    [Adharsh] Adds ability to find all accounts belonging to a user in user repository

--- src/main/kotlin/com/project/socialconnect/repositories/UserRespository.kt
+++ src/main/kotlin/com/project/socialconnect/repositories/UserRespository.kt
@@ -1,6 +1,7 @@
 package com.project.socialconnect.repositories
 
 
+import com.project.socialconnect.models.Account
 import com.project.socialconnect.models.User
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -19,4 +20,8 @@ interface UserRepository : CrudRepository<User, Long> {
     fun updateFirstAndLastName(@Param("userId") userId: Long,
                                @Param("firstName") firstName: String,
                                @Param("lastName") lastName: String): Int
+
+    @Transactional
+    @Query("SELECT accounts.id, accounts.name, accounts.account_type_id FROM accounts INNER JOIN users_accounts ON users_accounts.user_id = ?1 AND users_accounts.accounts_id = accounts.id", nativeQuery = true)
+    fun findAllAccountsByUserId(userId: Long): Array<Array<Any>>
 }
