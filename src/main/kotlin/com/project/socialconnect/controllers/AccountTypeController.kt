ec98c86e82ff590933815bce62bd96218611faf9
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-11 14:16:17 +0530

    [Adharsh] Refactors controller methods in Account Type controller

--- src/main/kotlin/com/project/socialconnect/controllers/AccountTypeController.kt
+++ src/main/kotlin/com/project/socialconnect/controllers/AccountTypeController.kt
@@ -1,7 +1,10 @@
 package com.project.socialconnect.controllers
 
+import com.project.socialconnect.composers.ResponseComposer
+import com.project.socialconnect.constants.ErrorConstants
 import com.project.socialconnect.models.AccountType
 import com.project.socialconnect.repositories.AccountTypeRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
@@ -10,22 +13,30 @@ class AccountTypeController(private val accountTypeRepository: AccountTypeReposi
 
     @PostMapping("/account-type")
     @ResponseBody
-    fun createAccountType(@RequestBody accountType: AccountType): AccountType {
-        val newAccountType = AccountType(accountType.getName(), accountType.getDescription())
-        accountTypeRepository.save(newAccountType)
-        return newAccountType
+    fun createAccountType(@RequestBody accountType: AccountType): ResponseEntity<Any> {
+        val newAccountType = accountTypeRepository.save(AccountType(accountType.getName(), accountType.getDescription()))
+        return ResponseComposer.composeSuccessResponseWith(newAccountType)
     }
 
     @GetMapping("/account-types")
     @ResponseBody
-    fun listAccountTypes(): List<AccountType> {
-        return accountTypeRepository.findAll().map { it }
+    fun listAccountTypes(): ResponseEntity<Any> {
+        val accountTypes = accountTypeRepository.findAll().map { it }
+        return ResponseComposer.composeSuccessResponseWith(accountTypes)
     }
 
-    @DeleteMapping("account-types/{id}")
+    @PutMapping("account-types/{id}")
     @ResponseBody
-    fun deleteAccountType(@PathVariable("id") id: Long): String {
-        accountTypeRepository.deleteById(id)
-        return "Deleted"
+    fun updateAccountType(@RequestBody accountType: AccountType, @PathVariable("id") id: Long): ResponseEntity<Any> {
+        val updatedAccountType = accountTypeRepository.findById(id)
+        return when {
+            !updatedAccountType.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_TYPE_NOT_FOUND)
+            else -> {
+                updatedAccountType.get().setName(accountType.getName())
+                updatedAccountType.get().setDescription(accountType.getDescription())
+                val accountTypeAfterUpdate = accountTypeRepository.save(updatedAccountType.get())
+                ResponseComposer.composeSuccessResponseWith(accountTypeAfterUpdate)
+            }
+        }
     }
 }
