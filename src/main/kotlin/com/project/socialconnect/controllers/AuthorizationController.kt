4299e526a286a0a6b2c333e38befe3e1fafdd2f6
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-11 16:44:46 +0530

    [Adharsh] Refactors authorizeAccount method in authorization controller

--- src/main/kotlin/com/project/socialconnect/controllers/AuthorizationController.kt
+++ src/main/kotlin/com/project/socialconnect/controllers/AuthorizationController.kt
@@ -1,9 +1,12 @@
 package com.project.socialconnect.controllers
 
 import com.project.socialconnect.authenticators.GoogleAuthenticator
+import com.project.socialconnect.composers.ResponseComposer
+import com.project.socialconnect.constants.ErrorConstants
 import com.project.socialconnect.models.AccountCredential
 import com.project.socialconnect.repositories.AccountCredentialRepository
 import com.project.socialconnect.repositories.AccountRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
@@ -15,18 +18,24 @@ class AuthorizationController(
 
     @GetMapping("/accounts/{accountId}/authorize")
     @ResponseBody
-    fun authorizeAccount(@PathVariable("accountId") accountId: Long): AccountCredential? {
-        val accountCredential = accountCredentialRepository.findByAccountId(accountId)
+    fun authorizeAccount(@PathVariable("accountId") accountId: Long): ResponseEntity<Any> {
         val account = accountRepository.findById(accountId)
         return when {
-            !account.isPresent -> null
-            !accountCredential.isPresent -> null
+            !account.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_FOUND)
             else -> {
-                googleAuthenticator.authorize()
-                val credential = googleAuthenticator.getCredential()
-                val newAccountCredential = AccountCredential(account.get(), credential.accessToken, credential.refreshToken,
-                        credential.expiresInSeconds, "Bearer")
-                accountCredentialRepository.save(newAccountCredential)
+                val accountCredential = accountCredentialRepository.findByAccountId(accountId)
+                return when {
+                    accountCredential.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_ALREADY_AUTHORIZED)
+                    else -> {
+                        googleAuthenticator.authorize()
+                        val credential = googleAuthenticator.getCredential()
+                        val newAccountCredential = AccountCredential(account.get(), credential.accessToken, credential.refreshToken,
+                                credential.expiresInSeconds, "Bearer")
+                        val accountCredentialAfterSave = accountCredentialRepository.save(newAccountCredential)
+                        ResponseComposer.composeSuccessResponseWith(accountCredentialAfterSave)
+
+                    }
+                }
             }
         }
     }
