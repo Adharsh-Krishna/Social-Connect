589c9765d376fef76e9162bbf364a6cfdb95343c
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-11 16:41:52 +0530

    [Adharsh] Modifies list files and adds method to create and download file to google drive

--- src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
+++ src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
@@ -2,13 +2,22 @@ package com.project.socialconnect.services
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.AbstractInputStreamContent
+import com.google.api.client.http.FileContent
+import com.google.api.client.http.InputStreamContent
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.IOUtils
 import com.google.api.services.drive.Drive
+import com.google.api.services.drive.model.File
 import com.google.api.services.drive.model.FileList
 import com.project.socialconnect.constants.GoogleDriveConstants
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
 
 @Service
 class GoogleDriveService {
@@ -34,18 +43,26 @@ class GoogleDriveService {
         }
     }
 
-    private fun listFiles() {
+    fun listFiles(pageSize: Int = GoogleDriveConstants.DEFAULT_FILES_PER_PAGE): MutableList<File> {
         val result: FileList = service.files().list()
-                .setPageSize(10)
-                .setFields("nextPageToken, files(id, name)")
+                .setPageSize(pageSize)
                 .execute()
-        val files: MutableList<com.google.api.services.drive.model.File>? = result.getFiles()
-        if (files == null || files.isEmpty()) {
-            System.out.println("No files found.")
-        } else {
-            System.out.println("Files:")
-            files.map {file -> System.out.printf("%s (%s)\n", file.getName(), file.getId()) }
-        }
+        return result.files!!
+    }
+
+    fun createFile(file: MultipartFile, fileName: String, contentType: String): File  {
+        val fileStream: InputStream = ByteArrayInputStream(file.bytes)
+        val newFile = File()
+        newFile.name = fileName
+        return  service
+                .files()
+                .create(newFile, InputStreamContent(contentType, fileStream))
+                .execute()
+    }
+
+    fun downloadFile(fileId: String) {
+        val outputStream = ByteArrayOutputStream()
+        return service.files().get(fileId).executeMediaAndDownloadTo(outputStream)
     }
 
 }
