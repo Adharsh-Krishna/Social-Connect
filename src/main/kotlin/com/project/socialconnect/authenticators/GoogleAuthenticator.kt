5a61d56c3d515d24bc2cf940e71fe0666a3c33cc
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-15 21:13:33 +0530

    [Adharsh] Refactors google authenticator and relpaces authorization code flow with authorization requests

--- src/main/kotlin/com/project/socialconnect/authenticators/GoogleAuthenticator.kt
+++ src/main/kotlin/com/project/socialconnect/authenticators/GoogleAuthenticator.kt
@@ -1,19 +1,19 @@
 package com.project.socialconnect.authenticators
 
-import com.google.api.client.auth.oauth2.BearerToken
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.auth.oauth2.*
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.BasicAuthentication
+import com.google.api.client.http.GenericUrl
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.drive.DriveScopes
 import com.project.socialconnect.constants.GoogleDriveConstants
+import com.project.socialconnect.models.Account
+import com.project.socialconnect.models.AccountCredential
 import org.springframework.stereotype.Service
 import java.io.File
 import java.io.InputStream
@@ -21,38 +21,46 @@ import java.io.InputStreamReader
 import java.util.*
 
 @Service
-class GoogleAuthenticator: Authenticator {
+class GoogleAuthenticator: Authenticator() {
 
     private val  jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
     private val scopes: List<String> = Collections.singletonList(DriveScopes.DRIVE)
     private val  httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
-    private lateinit var credential: Credential
 
-    fun getCredential(): Credential {
-        return this.credential
-    }
-
-    override fun authorize() {
+    override fun authorize(accountId: Long) {
         val input: InputStream? = File(GoogleDriveConstants.CREDENTIALS_FILE_PATH).inputStream()
         val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, InputStreamReader(input))
-
-        val flow: GoogleAuthorizationCodeFlow =  GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, jsonFactory, clientSecrets, scopes)
-                .setAccessType(GoogleDriveConstants.ACCESS_TYPE)
+        authorizationUrl = AuthorizationRequestUrl(clientSecrets.details.authUri, clientSecrets.details.clientId, Arrays.asList("code"))
+                .setState(accountId.toString())
+                .setRedirectUri(clientSecrets.details.redirectUris.first())
+                .set("access_type", GoogleDriveConstants.ACCESS_TYPE)
+                .setScopes(scopes)
                 .build()
-        val receiver: LocalServerReceiver = LocalServerReceiver
-                .Builder()
-                .setPort(GoogleDriveConstants.LOCAL_SERVER_PORT)
-                .build()
-        credential = AuthorizationCodeInstalledApp(flow, receiver).authorize(GoogleDriveConstants.USER_ID)
     }
 
-    override fun reAuthorize(refreshToken: String) {
+    override fun returnAccountCredential(account: Account, code: String): AccountCredential {
+        val input: InputStream? = File(GoogleDriveConstants.CREDENTIALS_FILE_PATH).inputStream()
+        val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, InputStreamReader(input))
+        val credentials = AuthorizationCodeTokenRequest(httpTransport, jsonFactory, GenericUrl(clientSecrets.details.tokenUri), code)
+                .setRedirectUri(clientSecrets.details.redirectUris.first())
+                .setClientAuthentication(BasicAuthentication(clientSecrets.details.clientId, clientSecrets.details.clientSecret))
+                .execute()
+        return AccountCredential(account, credentials.accessToken, credentials.refreshToken, credentials.expiresInSeconds, credentials.tokenType)
+
+    }
+
+    override fun reAuthorizeAndReturnAccountCredential(accountCredential: AccountCredential): AccountCredential {
+        val refreshToken = accountCredential.getRefreshToken()
         val input: InputStream? = File(GoogleDriveConstants.CREDENTIALS_FILE_PATH).inputStream()
         val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, InputStreamReader(input))
         val clientId = clientSecrets.details.clientId
         val clientSecret = clientSecrets.details.clientSecret
         val response = GoogleRefreshTokenRequest(httpTransport, jsonFactory, refreshToken, clientId, clientSecret).execute()
-        credential = GoogleCredential().setAccessToken(response.accessToken)
+        accountCredential.setAccessToken(response.accessToken)
+        return accountCredential
+    }
+
+    override fun extractAccountId(state: String): String {
+        return state
     }
 }
