18cd26261d42792969748832a23985aa8391aa1e
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-15 21:16:00 +0530

    [Adharsh] Adds authenticator factory

--- src/main/kotlin/com/project/socialconnect/factories/AuthenticatorFactory.kt
+++ src/main/kotlin/com/project/socialconnect/factories/AuthenticatorFactory.kt
@@ -1,2 +1,30 @@
 package com.project.socialconnect.factories
 
+import com.project.socialconnect.authenticators.Authenticator
+import com.project.socialconnect.authenticators.DropboxAuthenticator
+import com.project.socialconnect.authenticators.GoogleAuthenticator
+
+abstract class AuthenticatorFactory {
+
+    companion object {
+        private val authenticators: Map<String, Authenticator> = mutableMapOf(
+                Pair("google", GoogleAuthenticator()),
+                Pair("dropbox", DropboxAuthenticator())
+        )
+
+        private val applicableReAuthenticators: Map<String, Boolean> = mutableMapOf(
+            Pair("google", true),
+            Pair("dropbox", false)
+        )
+
+        fun getAuthenticator(authenticatorName: String): Authenticator {
+            return authenticators.get(authenticatorName)!!
+        }
+
+        fun isReAuthenticationApplicable(accountType: String): Boolean {
+            return applicableReAuthenticators.get(accountType)!!
+        }
+
+    }
+
+}
