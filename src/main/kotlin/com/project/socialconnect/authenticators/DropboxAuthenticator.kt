b14a1999a66c5bbc79b43e3bbf49cc8753fb4958
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-15 21:14:27 +0530

    [Adharsh] Adds dropbox authenticator along with its constants file

--- src/main/kotlin/com/project/socialconnect/authenticators/DropboxAuthenticator.kt
+++ src/main/kotlin/com/project/socialconnect/authenticators/DropboxAuthenticator.kt
@@ -1,2 +1,47 @@
 package com.project.socialconnect.authenticators
 
+import com.dropbox.core.*
+import com.project.socialconnect.constants.DropboxConstants
+import com.project.socialconnect.models.Account
+import com.project.socialconnect.models.AccountCredential
+import org.mortbay.jetty.Request.getRequest
+import org.springframework.stereotype.Service
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpSession
+
+
+
+@Service
+class DropboxAuthenticator: Authenticator() {
+    private val appInfo: DbxAppInfo = DbxAppInfo.Reader.readFromFile(DropboxConstants.CREDENTIALS_FILE_PATH)
+    private val requestConfig = DbxRequestConfig(DropboxConstants.CLIENT_IDENTIFIER)
+    private val webAuth = DbxWebAuth(requestConfig, appInfo)
+
+    override fun authorize(accountId: Long) {
+        val session = httpRequest.getSession(true)
+        val key = DropboxConstants.DBX_SESSION_STORE_KEY
+
+        val dbxStandardSessionStore = DbxStandardSessionStore(session, key)
+        val webAuthRequest = DbxWebAuth.newRequestBuilder()
+                .withRedirectUri(DropboxConstants.REDIRECT_URI, dbxStandardSessionStore)
+                .withState(accountId.toString())
+                .build()
+        authorizationUrl = webAuth.authorize(webAuthRequest)
+    }
+
+    override fun returnAccountCredential(account: Account, code: String): AccountCredential {
+        val credentials = webAuth.finishFromCode(code, DropboxConstants.REDIRECT_URI)
+        return AccountCredential(account, credentials.accessToken)
+    }
+
+    override fun reAuthorizeAndReturnAccountCredential(accountCredential: AccountCredential) {
+    }
+
+    override fun getCredential() {
+    }
+
+    override fun extractAccountId(state: String): String {
+        val index = state.lastIndexOf("==")
+        return state.substring(index + 2)
+    }
+}
--- src/main/kotlin/com/project/socialconnect/constants/DropboxConstants.kt
+++ src/main/kotlin/com/project/socialconnect/constants/DropboxConstants.kt
@@ -1,2 +1,10 @@
 package com.project.socialconnect.constants
 
+abstract class DropboxConstants {
+    companion object {
+        const val CREDENTIALS_FILE_PATH = "dropbox-credentials.app"
+        const val CLIENT_IDENTIFIER = "Social-Connect"
+        const val DBX_SESSION_STORE_KEY = "dropbox-auth-csrf-token"
+        const val REDIRECT_URI = "http://localhost:8080/accounts/dropbox/code"
+    }
+}
