45729bc6e3f96065a727465a2f5df3c566c1e195
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-15 21:11:31 +0530

    [Adharsh] Makes authenticator an abstract class and adds certain methods and attributes

--- src/main/kotlin/com/project/socialconnect/authenticators/Authenticator.kt
+++ src/main/kotlin/com/project/socialconnect/authenticators/Authenticator.kt
@@ -1,6 +1,14 @@
 package com.project.socialconnect.authenticators
 
-interface Authenticator{
-    fun authorize(): Any
-    fun reAuthorize(refreshToken: String): Any
+import com.project.socialconnect.models.Account
+import com.project.socialconnect.models.AccountCredential
+import javax.servlet.http.HttpServletRequest
+
+abstract class Authenticator{
+    lateinit var authorizationUrl: String
+    lateinit var httpRequest: HttpServletRequest
+    abstract fun authorize(accountId: Long): Any
+    abstract fun reAuthorizeAndReturnAccountCredential(accountCredential: AccountCredential): Any
+    abstract fun returnAccountCredential(account: Account, code: String): Any
+    abstract fun extractAccountId(state: String): Any
 }
