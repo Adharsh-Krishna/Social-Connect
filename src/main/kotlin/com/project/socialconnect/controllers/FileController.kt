265c1d4b36a962b63fc2cd1dd5d4c3e5ef71987d
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-21 17:25:34 +0530

    [Adharsh] Adds method to download file and certain other improvements

--- src/main/kotlin/com/project/socialconnect/controllers/FileController.kt
+++ src/main/kotlin/com/project/socialconnect/controllers/FileController.kt
@@ -1,15 +1,17 @@
 package com.project.socialconnect.controllers
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.project.socialconnect.composers.ResponseComposer
 import com.project.socialconnect.constants.ErrorConstants
+import com.project.socialconnect.factories.CloudServiceFactory
 import com.project.socialconnect.repositories.AccountCredentialRepository
 import com.project.socialconnect.repositories.AccountRepository
-import com.project.socialconnect.services.GoogleDriveService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayOutputStream
+import javax.servlet.http.HttpServletResponse
+
 
 @Controller
 class FileController(private val accountRepository: AccountRepository,
@@ -17,7 +19,8 @@ class FileController(private val accountRepository: AccountRepository,
 
     @GetMapping("/accounts/{accountId}/files")
     @ResponseBody
-    fun listFiles(@PathVariable("accountId") accountId: Long): Any {
+    fun listFiles(@PathVariable("accountId") accountId: Long,
+                  @RequestParam("pageSize") pageSize: Int = 10): Any {
         val account = accountRepository.findById(accountId)
         return when {
             !account.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_FOUND)
@@ -26,10 +29,11 @@ class FileController(private val accountRepository: AccountRepository,
                 return when {
                     !accountCredential.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_AUTHORIZED)
                     else -> {
-                        val service: GoogleDriveService = GoogleDriveService.getSingletonInstance()
-                        val credential: GoogleCredential = GoogleCredential().setAccessToken(accountCredential.get().getAccessToken())
-                        service.setCredential(credential)
-                        val files = service.listFiles()
+                        val accountType = account.get().getAccountType()!!.getName()
+                        val accessToken = accountCredential.get().getAccessToken()
+                        val service = CloudServiceFactory.getCloudService(accountType!!)
+                        service.setCredential(accessToken)
+                        val files = service.listFiles(pageSize)
                         ResponseComposer.composeSuccessResponseWith(files)
                     }
                 }
@@ -42,7 +46,6 @@ class FileController(private val accountRepository: AccountRepository,
     @ResponseBody
     fun uploadFile(@RequestParam("file") file: MultipartFile,
                    @RequestParam("fileName") fileName: String,
-                   @RequestParam("contentType") contentType: String,
                    @PathVariable("accountId") accountId: Long): Any {
         val account = accountRepository.findById(accountId)
         return when {
@@ -52,10 +55,11 @@ class FileController(private val accountRepository: AccountRepository,
                 return when {
                     !accountCredential.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_AUTHORIZED)
                     else -> {
-                        val service: GoogleDriveService = GoogleDriveService.getSingletonInstance()
-                        val credential: GoogleCredential = GoogleCredential().setAccessToken(accountCredential.get().getAccessToken())
-                        service.setCredential(credential)
-                        val newFile = service.createFile(file, fileName, contentType)
+                        val accountType = account.get().getAccountType()!!.getName()
+                        val accessToken = accountCredential.get().getAccessToken()
+                        val service = CloudServiceFactory.getCloudService(accountType!!)
+                        service.setCredential(accessToken)
+                        val newFile = service.createFile(file, fileName)
                         ResponseComposer.composeSuccessResponseWith(newFile)
                     }
                 }
@@ -63,25 +67,35 @@ class FileController(private val accountRepository: AccountRepository,
         }
     }
 
-//    @GetMapping("/accounts/{accountId}/files/{fileId}/download")
-//    @ResponseBody
-//    fun downloadFile(@PathVariable("accountId") accountId: Long, @PathVariable("fileId") fileId: String): ResponseEntity<Any> {
-//        val account = accountRepository.findById(accountId)
-//        return when {
-//            !account.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_FOUND)
-//            else -> {
-//                val accountCredential = accountCredentialRepository.findByAccountId(accountId)
-//                return when {
-//                    !accountCredential.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_AUTHORIZED)
-//                    else -> {
-//                        val service: GoogleDriveService = GoogleDriveService.getSingletonInstance()
-//                        val credential: GoogleCredential = GoogleCredential().setAccessToken(accountCredential.get().getAccessToken())
-//                        service.setCredential(credential)
-//                        val downloadedFile = service.downloadFile(fileId)
-//                        ResponseComposer.composeSuccessResponseWith(downloadedFile)
-//                    }
-//                }
-//            }
-//        }
-//    }
+
+    @GetMapping("/accounts/{accountId}/files/download")
+    @ResponseBody
+    fun downloadFile(@PathVariable("accountId") accountId: Long,
+                     @RequestParam("fileId", required = false) fileId: String?,
+                     @RequestParam("fileName", required = false) fileName: String?,
+                     response: HttpServletResponse): ResponseEntity<Any> {
+        val account = accountRepository.findById(accountId)
+        return when {
+            !account.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_FOUND)
+            fileId.isNullOrBlank() && fileName.isNullOrBlank() -> ResponseComposer.composeErrorResponseWith(ErrorConstants.FILE_NAME_OR_FILE_ID_MUST_BE_PASSED)
+            else -> {
+                val accountCredential = accountCredentialRepository.findByAccountId(accountId)
+                return when {
+                    !accountCredential.isPresent -> ResponseComposer.composeErrorResponseWith(ErrorConstants.ACCOUNT_NOT_AUTHORIZED)
+                    else -> {
+                        val accountType = account.get().getAccountType()!!.getName()
+                        val service = CloudServiceFactory.getCloudService(accountType!!)
+                        val accessToken = accountCredential.get().getAccessToken()
+                        service.setCredential(accessToken)
+                        if (!service.checkIfFileExists(fileId = fileId, fileName = fileName)) {
+                            return ResponseComposer.composeErrorResponseWith(ErrorConstants.UNKNOWN_ERROR)
+                        }
+                        val downloadFile = service.downloadFile(fileId = fileId, fileName = fileName)!!
+                        val content = downloadFile.toByteArray()!!
+                        ResponseComposer.sendFileWithSuccessResponse("hah.txt", "text/plain", content)
+                    }
+                }
+            }
+        }
+    }
 }
