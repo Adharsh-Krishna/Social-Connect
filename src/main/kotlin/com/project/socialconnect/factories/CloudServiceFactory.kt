bcb322b8fa25a1e528ed9de50fa9be27f4d06e39
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-21 17:19:53 +0530

    [Adharsh] Adds cloud service factory

--- src/main/kotlin/com/project/socialconnect/factories/CloudServiceFactory.kt
+++ src/main/kotlin/com/project/socialconnect/factories/CloudServiceFactory.kt
@@ -1,2 +1,19 @@
 package com.project.socialconnect.factories
 
+import com.project.socialconnect.services.CloudService
+import com.project.socialconnect.services.DropboxService
+import com.project.socialconnect.services.GoogleDriveService
+
+interface CloudServiceFactory {
+
+    companion object {
+        private val cloudServices: Map<String, CloudService> = mutableMapOf(
+                Pair("google", GoogleDriveService()),
+                Pair("dropbox", DropboxService())
+        )
+
+        fun getCloudService(cloudServiceName: String): CloudService {
+            return cloudServices.get(cloudServiceName)!!
+        }
+    }
+}
