cdcde7c749160eddd0fe4e30d829ab35b1304958
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-21 17:21:21 +0530

    [Adharsh] Adds constants to list the number of files per page in dropbox

--- src/main/kotlin/com/project/socialconnect/constants/DropboxConstants.kt
+++ src/main/kotlin/com/project/socialconnect/constants/DropboxConstants.kt
@@ -6,5 +6,6 @@ abstract class DropboxConstants {
         const val CLIENT_IDENTIFIER = "Social-Connect"
         const val DBX_SESSION_STORE_KEY = "dropbox-auth-csrf-token"
         const val REDIRECT_URI = "http://localhost:8080/accounts/dropbox/code"
+        const val DEFAULT_FILES_PER_PAGE = 10;
     }
 }
