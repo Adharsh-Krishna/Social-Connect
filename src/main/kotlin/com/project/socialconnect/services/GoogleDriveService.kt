116c76495ffd07a462b08a94272e0f169377f713
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-21 17:25:05 +0530

    [Adharsh] Google drive service implements cloud service , removes singleton and certain other changes

--- src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
+++ src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
@@ -2,13 +2,10 @@ package com.project.socialconnect.services
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.http.AbstractInputStreamContent
-import com.google.api.client.http.FileContent
 import com.google.api.client.http.InputStreamContent
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.IOUtils
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.model.File
 import com.google.api.services.drive.model.FileList
@@ -17,52 +14,46 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.InputStream
 
 @Service
-class GoogleDriveService {
-    private val  jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
-    private val  httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+class GoogleDriveService: CloudService {
+    private val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
+    private val httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
     private lateinit var service: Drive
 
-    fun setCredential(credential: GoogleCredential) {
+    override fun setCredential(accessToken: String) {
+        val credential: GoogleCredential = GoogleCredential().setAccessToken(accessToken)
         this.service = Drive.Builder(httpTransport, jsonFactory, credential)
-            .setApplicationName(GoogleDriveConstants.APPLICATION_NAME)
-            .build()
+                .setApplicationName(GoogleDriveConstants.APPLICATION_NAME)
+                .build()
     }
 
-    companion object {
-        private var instance: GoogleDriveService? = null
-
-        @Synchronized
-        fun getSingletonInstance(): GoogleDriveService {
-            if (instance == null) {
-                instance = GoogleDriveService()
-            }
-            return instance!!
-        }
-    }
-
-    fun listFiles(pageSize: Int = GoogleDriveConstants.DEFAULT_FILES_PER_PAGE): MutableList<File> {
+    override fun listFiles(pageSize: Int): MutableList<File> {
         val result: FileList = service.files().list()
                 .setPageSize(pageSize)
                 .execute()
         return result.files!!
     }
 
-    fun createFile(file: MultipartFile, fileName: String, contentType: String): File  {
-        val fileStream: InputStream = ByteArrayInputStream(file.bytes)
+    override fun createFile(file: MultipartFile, fileName: String): File {
+        val fileStream = ByteArrayInputStream(file.bytes)
+        val contentType = file.contentType!!
         val newFile = File()
         newFile.name = fileName
-        return  service
+        return service
                 .files()
                 .create(newFile, InputStreamContent(contentType, fileStream))
                 .execute()
     }
 
-    fun downloadFile(fileId: String) {
+    override fun downloadFile(fileId: String?, fileName: String?): ByteArrayOutputStream? {
         val outputStream = ByteArrayOutputStream()
-        return service.files().get(fileId).executeMediaAndDownloadTo(outputStream)
+        service.files().get(fileId).executeMediaAndDownloadTo(outputStream)
+        return outputStream
     }
 
+    override fun checkIfFileExists(fileId: String?, fileName: String?): Boolean {
+        return true
+    }
 }
+
