bbe6308f61cf4547efbf33a56b4db31902d009d6
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-22 16:59:17 +0530

    [Adharsh] Adds provision to transfer files in google drive service

--- src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
+++ src/main/kotlin/com/project/socialconnect/services/GoogleDriveService.kt
@@ -10,13 +10,17 @@ import com.google.api.services.drive.Drive
 import com.google.api.services.drive.model.File
 import com.google.api.services.drive.model.FileList
 import com.project.socialconnect.constants.GoogleDriveConstants
+import com.project.socialconnect.models.AccountCredential
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import javax.activation.MimetypesFileTypeMap
 
 @Service
-class GoogleDriveService: CloudService {
+class GoogleDriveService: CloudService() {
     private val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
     private val httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
     private lateinit var service: Drive
@@ -30,18 +34,25 @@ class GoogleDriveService: CloudService {
 
     override fun listFiles(pageSize: Int): MutableList<File> {
         val result: FileList = service.files().list()
+                .setFields("nextPageToken, files(id, name, parents, kind, mimeType)")
+                .setQ("mimeType != 'application/vnd.google-apps.folder'")
                 .setPageSize(pageSize)
                 .execute()
         return result.files!!
     }
 
-    override fun createFile(file: MultipartFile, fileName: String): File {
+    override fun listAllFolders(folderId: String?, folderPath: String?): Any {
+       return  service.files().list()
+                .setQ("mimeType = 'application/vnd.google-apps.folder' and '$folderId' in parents")
+                .execute().files
+    }
+
+    override fun uploadFile(file: MultipartFile, fileName: String): File {
         val fileStream = ByteArrayInputStream(file.bytes)
-        val contentType = file.contentType!!
+        val contentType = MimetypesFileTypeMap.getDefaultFileTypeMap().getContentType(fileName)!!
         val newFile = File()
         newFile.name = fileName
-        return service
-                .files()
+        return service.files()
                 .create(newFile, InputStreamContent(contentType, fileStream))
                 .execute()
     }
@@ -55,5 +66,31 @@ class GoogleDriveService: CloudService {
     override fun checkIfFileExists(fileId: String?, fileName: String?): Boolean {
         return true
     }
+
+    override fun deleteFile(fileId: String?, fileName: String?): Any {
+        service.files().delete(fileId).execute()
+        return "deleted"
+    }
+
+    override fun transferToDropbox(accountCredential: AccountCredential, fileId: String?, folderPath: String?, folderId: String?): Any {
+        val outputStream = downloadFile(fileId)!!
+        val dropboxService = DropboxService()
+        val accessToken = accountCredential.getAccessToken()
+        dropboxService.setCredential(accessToken)
+        val googleFileDetails = getFileDetails(fileId, null)
+        val multipartFile = MockMultipartFile(googleFileDetails.name, outputStream.toByteArray())
+        val uploadedFile = dropboxService.uploadFile(multipartFile, folderPath + "/" + googleFileDetails.name)
+        deleteFile(fileId, null)
+        return uploadedFile
+    }
+
+    override fun getFileDetails(fileId: String?, fileName: String?): File{
+        return service.files().get(fileId).execute()
+    }
+
+    override fun transferToGoogleDrive(accountCredential: AccountCredential, fileName: String?, folderPath: String?, folderId: String?): Any {
+        return ""
+    }
+
 }
 
