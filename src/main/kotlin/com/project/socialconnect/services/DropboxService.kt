4b44894ffe5b27169adcbb872f33a687fb1e3e8c
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-22 16:59:38 +0530

    [Adharsh] Adds provision to transfer files in dropbox service

--- src/main/kotlin/com/project/socialconnect/services/DropboxService.kt
+++ src/main/kotlin/com/project/socialconnect/services/DropboxService.kt
@@ -2,19 +2,26 @@ package com.project.socialconnect.services
 
 import com.dropbox.core.DbxRequestConfig
 import com.dropbox.core.v2.DbxClientV2
+import com.dropbox.core.v2.files.FolderMetadata
+import com.dropbox.core.v2.files.GetMetadataErrorException
 import com.dropbox.core.v2.files.Metadata
 import com.project.socialconnect.constants.DropboxConstants
+import com.project.socialconnect.models.AccountCredential
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
-import java.io.OutputStream
-import com.dropbox.core.v2.files.GetMetadataErrorException
-
+import java.nio.file.Files
+import java.nio.file.Path
 
 
 @Service
-class DropboxService: CloudService {
+class DropboxService: CloudService() {
+    override fun getFileDetails(fileId: String?, fileName: String?): Metadata {
+        return service.files().getMetadata(fileName)
+    }
+
     private val requestConfig = DbxRequestConfig(DropboxConstants.CLIENT_IDENTIFIER)
     private lateinit var service: DbxClientV2
 
@@ -28,7 +35,7 @@ class DropboxService: CloudService {
     }
 
     override fun checkIfFileExists(fileId: String?, fileName: String?): Boolean {
-        try {
+        val metadata: Metadata = try {
             service.files().getMetadata(fileName)
         } catch (e: GetMetadataErrorException) {
             return !(e.errorValue.isPath && e.errorValue.pathValue.isNotFound)
@@ -36,7 +43,7 @@ class DropboxService: CloudService {
         return true
     }
 
-    override fun createFile(file: MultipartFile, fileName: String): Any {
+    override fun uploadFile(file: MultipartFile, fileName: String): Any {
         val inputStream = BufferedInputStream(file.inputStream)
         return service
                 .files()
@@ -49,4 +56,33 @@ class DropboxService: CloudService {
         service.files().downloadBuilder(fileName).download(outputStream)
         return outputStream
     }
+
+    override fun deleteFile(fileId: String?, fileName: String?): Any {
+        return service.files().deleteV2(fileName)
+    }
+
+    override fun transferToDropbox(accountCredential: AccountCredential, fileId: String?, folderPath: String?, folderId: String?): Any {
+        return ""
+    }
+
+    override fun listAllFolders(folderId: String?, folderPath: String?): Any {
+        val actualFolderPath = if (folderPath == "root") "" else folderPath
+        return service.files()
+                .listFolder(actualFolderPath)
+                .entries
+                .takeWhile { it is FolderMetadata }
+    }
+
+    override fun transferToGoogleDrive(accountCredential: AccountCredential, fileName: String?, folderPath: String?, folderId: String?): Any {
+        val accessToken = accountCredential.getAccessToken()
+        val googleDriveService = GoogleDriveService()
+        googleDriveService.setCredential(accessToken)
+        val outputStream = downloadFile(null, fileName)!!
+        val dropboxFileDetails = getFileDetails(null, fileName)
+        val multipartFile = MockMultipartFile(dropboxFileDetails.name, outputStream.toByteArray())
+        val transferredFile = googleDriveService.uploadFile(multipartFile, dropboxFileDetails.name)
+        deleteFile(null, fileName)
+        return transferredFile
+    }
+
 }
