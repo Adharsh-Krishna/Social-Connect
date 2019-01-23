696f54cf8c1d869e734eb773b22709a95aec1575
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-23 16:44:41 +0530

    [Adharsh] Adds ROOT FOLDER constant

--- src/main/kotlin/com/project/socialconnect/constants/AppConstants.kt
+++ src/main/kotlin/com/project/socialconnect/constants/AppConstants.kt
@@ -1,6 +1,7 @@
 package com.project.socialconnect.constants
 abstract class AppConstants {
     companion object {
+        const val ROOT_FOLDER = "root"
         const val GOOGLE = "google"
         const val DROPBOX = "dropbox"
         val accountTypes = listOf<String>(GOOGLE, DROPBOX)
