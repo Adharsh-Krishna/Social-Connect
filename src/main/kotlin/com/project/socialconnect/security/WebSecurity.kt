6686c4b5e532e9cebdc6a31572a6bffa86ae52fc
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-18 16:39:55 +0530

    [Adharsh] Adds Spring web security

--- src/main/kotlin/com/project/socialconnect/security/WebSecurity.kt
+++ src/main/kotlin/com/project/socialconnect/security/WebSecurity.kt
@@ -1,6 +1,6 @@
-package com.project.socialconnect.auth
+package com.project.socialconnect.security
 
-import com.project.socialconnect.auth.SecurityConstants.SIGN_UP_URL
+import com.project.socialconnect.security.SecurityConstants.SIGN_UP_URL
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
