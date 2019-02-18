1572f848a7b6063627628a11a77c4b27ab5fbcd7
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-18 12:45:21 +0530

    [Adharsh] Adds fakeit in build gradle and starts unit testing of user model

---
+++ src/test/kotlin/com/project/socialconnect/models/UserTest.kt
@@ -0,0 +1,21 @@
+package com.project.socialconnect.models
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+class UserTest {
+    init {
+
+    }
+
+    @Test fun shouldCreateNewUser() {
+        val firstName = "testFirstName"
+        val lastName = "testLastName"
+        val userName = "testUserName"
+        val accounts = null
+        val newUser = User(firstName, lastName, userName)
+        assertEquals(newUser.getAccounts(), accounts)
+        assertEquals(newUser.getUserName(), userName)
+        assertEquals(newUser.getLastName(), lastName)
+        assertEquals(newUser.getFirstName(), firstName)
+    }
+}
