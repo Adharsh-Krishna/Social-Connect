223b14ee399ad48dabb84886ec83db91f05ad71c
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:49:40 +0530

    [Adharsh] Adds role repository

--- src/main/kotlin/com/project/socialconnect/repositories/RoleRepository.kt
+++ src/main/kotlin/com/project/socialconnect/repositories/RoleRepository.kt
@@ -1,7 +1,11 @@
 package com.project.socialconnect.repositories;
 
+import com.project.socialconnect.models.Role
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
-
+interface RoleRepository: CrudRepository<Role, Long> {
+    fun findByName(name: String): Optional<Role>
+}
 
 
