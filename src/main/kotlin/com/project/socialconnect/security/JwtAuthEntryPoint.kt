6d5588c9d1cc3c3a8c5502bb1b6368279d05086c
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:55:08 +0530

    [Adharsh] Adds Jwt Auth Entry Point to handle exceptions - auth failed

--- src/main/kotlin/com/project/socialconnect/security/JwtAuthEntryPoint.kt
+++ src/main/kotlin/com/project/socialconnect/security/JwtAuthEntryPoint.kt
@@ -1,2 +1,20 @@
 package com.project.socialconnect.security
 
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Service
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Service
+class JwtAuthEntryPoint: AuthenticationEntryPoint {
+
+    private val logger = LoggerFactory.getLogger(JwtAuthEntryPoint::class.java)
+
+    override fun commence(request: HttpServletRequest?, response: HttpServletResponse?, authException: AuthenticationException?) {
+        logger.error("Unauthorized error. Message - {}", authException)
+        response!!.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized");
+    }
+
+}
