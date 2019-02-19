479355ed4263fed7fc417f8b432c985ffcef63ee
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:51:42 +0530

    [Adharsh] Adds SignInRequest model

--- src/main/kotlin/com/project/socialconnect/payloads/auth/SignInRequest.kt
+++ src/main/kotlin/com/project/socialconnect/payloads/auth/SignInRequest.kt
@@ -1,2 +1,14 @@
 package com.project.socialconnect.payloads.auth
 
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+class SignInRequest {
+    @NotBlank
+    @Size(min = 3, max = 60)
+    var userName: String? = null
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    var password: String? = null
+}
