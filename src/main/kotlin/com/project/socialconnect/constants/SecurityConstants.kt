8c70050b6779d4276cecd05ad36dbcd994d537b9
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:51:13 +0530

    [Adharsh] Adds SIGN IN url to the security constants and reduces token expiration time

--- src/main/kotlin/com/project/socialconnect/constants/SecurityConstants.kt
+++ src/main/kotlin/com/project/socialconnect/constants/SecurityConstants.kt
@@ -1,9 +1,10 @@
-package com.project.socialconnect.security
+package com.project.socialconnect.constants
 
 object SecurityConstants {
     const val SECRET = "SecretKeyToGenJWTs"
-    const val EXPIRATION_TIME: Long = 864000000 // 10 days
+    const val EXPIRATION_TIME: Long = 86400
     const val TOKEN_PREFIX = "Bearer "
     const val HEADER_STRING = "Authorization"
     const val SIGN_UP_URL = "/users/sign-up"
+    const val SIGN_IN_URL = "/users/sign-in"
 }
