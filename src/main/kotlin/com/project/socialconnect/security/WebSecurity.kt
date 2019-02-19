97b3da736084737c9efb019cb54cb36af72ad519
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:54:13 +0530

    [Adharsh] Updates WebSecurity , Introduces session mgmt, auth entry point and disables cors/csrf

--- src/main/kotlin/com/project/socialconnect/security/WebSecurity.kt
+++ src/main/kotlin/com/project/socialconnect/security/WebSecurity.kt
@@ -1,30 +1,44 @@
 package com.project.socialconnect.security
 
-import com.project.socialconnect.security.SecurityConstants.SIGN_UP_URL
+import com.project.socialconnect.constants.SecurityConstants.SIGN_IN_URL
+import com.project.socialconnect.constants.SecurityConstants.SIGN_UP_URL
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @EnableWebSecurity
-class WebSecurity(private val userDetailsService: UserDetailsServiceImpl) : WebSecurityConfigurerAdapter() {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurity(private val userDetailsService: UserDetailsServiceImpl,
+                  private val entryPoint: JwtAuthEntryPoint) : WebSecurityConfigurerAdapter() {
+
     private val bCryptPasswordEncoder: BCryptPasswordEncoder = BCryptPasswordEncoder()
+    private val authenticationJwtTokenFilter: JwtAuthTokenFilter = JwtAuthTokenFilter(userDetailsService)
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, SIGN_IN_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .addFilter(JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(JWTAuthorizationFilter(authenticationManager()))
+                .exceptionHandling().authenticationEntryPoint(entryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+
     }
 
     @Throws(Exception::class)
-    public override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth!!.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder)
+    public override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder?) {
+        authenticationManagerBuilder!!
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
     }
 }
