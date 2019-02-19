29d4250da2430898cd597a388c5e68922f4706bd
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:52:14 +0530

    [Adharsh] Adds SignUpRequest Model

--- src/main/kotlin/com/project/socialconnect/payloads/auth/SignUpRequest.kt
+++ src/main/kotlin/com/project/socialconnect/payloads/auth/SignUpRequest.kt
@@ -1,2 +1,25 @@
 package com.project.socialconnect.payloads.auth
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    var firstName: String? = null
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    var lastName: String? = null
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    var userName: String? = null
+
+    var roles: MutableList<String>? = null
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    var password: String? = null
+}
