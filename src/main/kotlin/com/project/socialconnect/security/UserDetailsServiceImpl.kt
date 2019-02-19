412db8c396442a774b5d383dd8d134a8c5c94680
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:57:02 +0530

    [Adharsh] Updates loadByUserName by returning user details wrapped inside user principle

--- src/main/kotlin/com/project/socialconnect/security/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/project/socialconnect/security/UserDetailsServiceImpl.kt
@@ -1,17 +1,18 @@
 package com.project.socialconnect.security
 
 import com.project.socialconnect.repositories.UserRepository
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
+class UserDetailsServiceImpl(private val userRepository: UserRepository): UserDetailsService {
+
     @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(username: String): UserDetails {
-        val applicationUser = userRepository.findByUserName(username).get()
-        return User(applicationUser.getUserName(), applicationUser.getPassword(), emptyList())
+        val applicationUser = userRepository.findByUserName(username)
+                .orElseThrow { UsernameNotFoundException("User Name not found") }
+        return UserPrinciple.build(applicationUser)
     }
 }
