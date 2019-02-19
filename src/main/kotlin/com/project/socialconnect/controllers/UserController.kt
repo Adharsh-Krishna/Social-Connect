2e55633788116d622483e3b9faa0a3b4b81b464b
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-02-19 15:58:34 +0530

    [Adharsh] Adds sign-in and sign-up endpoint and enables jwt authentication in the user controller

--- src/main/kotlin/com/project/socialconnect/controllers/UserController.kt
+++ src/main/kotlin/com/project/socialconnect/controllers/UserController.kt
@@ -1,26 +1,35 @@
 package com.project.socialconnect.controllers
 
 import com.project.socialconnect.composers.ResponseComposer
-import com.project.socialconnect.models.User
 import com.project.socialconnect.constants.ErrorConstants
+import com.project.socialconnect.models.User
+import com.project.socialconnect.payloads.auth.SignInRequest
+import com.project.socialconnect.payloads.auth.SignUpRequest
+import com.project.socialconnect.payloads.jwt.JwtResponse
+import com.project.socialconnect.repositories.RoleRepository
 import com.project.socialconnect.repositories.UserRepository
+import com.project.socialconnect.security.JwtProvider
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.mortbay.jetty.security.Password.getPassword
-import org.springframework.web.bind.annotation.RequestBody
-
-
-
+import javax.validation.Valid
 
 
 @Controller
-class UserController(private val userRepository: UserRepository){
+class UserController(private val userRepository: UserRepository,
+                     private val roleRepository: RoleRepository){
 
-    private val bCryptPasswordEncoder: BCryptPasswordEncoder = BCryptPasswordEncoder()
+    private val bCryptPasswordEncoder = BCryptPasswordEncoder()
+    private val jwtProvider: JwtProvider = JwtProvider()
+
 
     @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseBody
     fun listUsers(): ResponseEntity<Any> {
         val users = userRepository.findAll().map { it.toUserResponsePayload()}
@@ -28,6 +37,7 @@ class UserController(private val userRepository: UserRepository){
     }
 
     @GetMapping("/users/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @ResponseBody
     fun getUser(@PathVariable("id") id: Long): ResponseEntity<Any> {
         val user = userRepository.findById(id)
@@ -37,21 +47,41 @@ class UserController(private val userRepository: UserRepository){
         }
     }
 
+
     @PostMapping("/users/sign-up")
     @ResponseBody
-    fun createUser(@RequestBody user: User): ResponseEntity<Any> {
-        val existingUser = userRepository.findByUserName(user.getUserName()!!)
-        return when {
-            existingUser.isPresent -> ResponseComposer.composeSuccessResponseWith(ErrorConstants.USER_ALREADY_EXISTS_WITH_USER_NAME)
-            else -> {
-                val encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword())
-                val newUser = userRepository.save(User(user.getFirstName(), user.getLastName(), user.getUserName(), encryptedPassword))
-                ResponseComposer.composeSuccessResponseWith(newUser)
-            }
+    fun registerUser(@Valid @RequestBody signUpRequest: SignUpRequest): ResponseEntity<Any> {
+        if (!userRepository.existsByUserName(signUpRequest.userName!!).isPresent) {
+            return ResponseComposer.composeErrorResponseWith("User Name is already present")
         }
+
+        val roleNames = signUpRequest.roles!!
+        val roles = roleNames.map { name ->  roleRepository.findByName(name).get()}
+        val user = User(signUpRequest.firstName, signUpRequest.lastName, signUpRequest.userName,
+                bCryptPasswordEncoder.encode(signUpRequest.password), roles.toSet())
+        userRepository.save(user)
+
+        return ResponseComposer.composeSuccessResponseWith(user)
+    }
+
+    @PostMapping("/users/sign-in")
+    @ResponseBody
+    fun authenticateUser(@Valid @RequestBody loginRequest: SignInRequest): ResponseEntity<Any> {
+
+        val authenticationManager =  AuthenticationManager { it }
+        val authentication = authenticationManager.authenticate(
+                UsernamePasswordAuthenticationToken(
+                        loginRequest.userName,
+                        loginRequest.password))
+
+        SecurityContextHolder.getContext().authentication = authentication
+
+        val jwt = jwtProvider.generateJwtToken(authentication)
+        return ResponseEntity.ok(JwtResponse(jwt))
     }
 
     @PutMapping("/users/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @ResponseBody
     fun updateUser(@RequestBody user: User, @PathVariable("id") id: Long): ResponseEntity<Any> {
         val existingUser = userRepository.findById(id)
